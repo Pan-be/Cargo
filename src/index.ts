1e9253dfd9d597e1e7d644cc46f0d94bccdc0fbf
Konrad <konradbieniek@gmail.com>
2023-03-13 22:11:23 +0100

    added markrers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { Destination } from "./Destination"
+import { Cargo } from "./Cargo"
 import { TrackingMap } from "./TrackingMap"
-
-const trackingMap = new TrackingMap("g-map")
 
 const destination = new Destination()
 
 console.log(destination)
+
+const cargo = new Cargo()
+
+const trackingMap = new TrackingMap("g-map")
+trackingMap.attachMarker(destination)
+trackingMap.attachMarker(cargo)
